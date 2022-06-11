4c69c3e838718f9ed38eef6121608043c63f168b
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-06-12 01:36:34 +0300

    Create Next project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Expenseer</title>
+        <meta name="description" content="Expenseer" />
+      </Head>
+      <h1>Expenseer</h1>
+    </div>
+  )
+}
