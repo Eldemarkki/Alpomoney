8cb1d3af3f32c8056c28c35079b6039779879e8f
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-06-24 02:43:26 +0300

    Create sidebar, add custom font

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,28 +1,103 @@
 import Link from "next/link";
-import { PropsWithChildren } from "react";
-import styled from "styled-components";
+import { useRouter } from "next/router";
+import { PropsWithChildren, ReactNode } from "react";
+import styled, { createGlobalStyle } from "styled-components";
 
-const Container = styled.div`
-  padding: 30px;
-`;
+const GlobalStyles = createGlobalStyle({
+  ":root": {
+    "--background-color": "#eaecef",
+    "--sidebar-top-color": "#010516",
+    "--sidebar-bottom-color": "#0a0e1e"
+  }
+});
 
-const Header = styled.header`
-  display: flex;
-  gap: 16px;
-`;
+const Container = styled.div({
+  display: "flex",
+  gap: 30,
+  height: "100vh"
+});
+
+const Sidebar = styled.div({
+  display: "flex",
+  justifyContent: "space-between",
+  flexDirection: "column",
+  width: 300,
+  margin: 16,
+  borderRadius: 16,
+  background: "linear-gradient(to bottom, var(--sidebar-top-color), var(--sidebar-bottom-color))"
+});
+
+const SidebarTop = styled.div({
+  display: "flex",
+  padding: 35,
+  gap: 16,
+  flexDirection: "column"
+});
+
+const SidebarBottom = styled.div({
+  color: "white",
+  padding: 15
+});
+
+const NewTransactionButton = styled.button({
+  backgroundColor: "rgb(145, 202, 253)",
+  padding: 15,
+  borderRadius: 8,
+  textAlign: "center",
+  color: "black",
+  cursor: "pointer",
+  border: "none",
+  width: "100%"
+});
+
+const StyledLink = styled.a<{ active?: boolean }>(props => ({
+  color: props.active ? "white" : "#999",
+  textDecoration: "none",
+  transitionDuration: "200ms",
+  "&:hover": {
+    color: "#DDD"
+  }
+}));
+
+interface StyledNextLinkProps {
+  href: string,
+  children: ReactNode
+}
+
+const StyledNextLink = (props: StyledNextLinkProps) => {
+  const router = useRouter();
+  return <Link href={props.href} passHref>
+    <StyledLink active={router.pathname === props.href}>
+      {props.children}
+    </StyledLink>
+  </Link>;
+};
 
 export const Layout = (props: PropsWithChildren) => {
   return <Container>
-    <Header>
-      <Link href="/">Dashboard</Link>
-      <Link href="/transactions">Transactions</Link>
-      <Link href="/recurring-transactions">Recurring transactions</Link>
-      <Link href="/storages">Storages</Link>
-      <Link href="/sinks">Sinks</Link>
-      <Link href="/login">Login</Link>
-      <Link href="/register">Register</Link>
-      <Link href="/api/logout">Log out</Link>
-    </Header>
+    <GlobalStyles />
+    <Sidebar>
+      <SidebarTop>
+        <StyledNextLink href="/">🏠 Dashboard</StyledNextLink>
+        <hr />
+        <StyledNextLink href="/transactions">💸 Transactions</StyledNextLink>
+        <StyledNextLink href="/recurring-transactions">🔁 Recurring transactions</StyledNextLink>
+        <hr />
+        <StyledNextLink href="/storages">🏦 Storages</StyledNextLink>
+        <StyledNextLink href="/sinks">🛒 Sinks</StyledNextLink>
+        <hr />
+        <StyledNextLink href="/login">🔒 Login</StyledNextLink>
+        <StyledNextLink href="/register">➕ Register</StyledNextLink>
+        <StyledNextLink href="/api/logout">🔐 Log out</StyledNextLink>
+      </SidebarTop>
+      <SidebarBottom>
+        <NewTransactionButton onClick={() => {
+          console.log("TODO: open new transaction form");
+        }}>
+          New transaction
+        </NewTransactionButton>
+      </SidebarBottom>
+    </Sidebar>
     <main>
       {props.children}
     </main>
