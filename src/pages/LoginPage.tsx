d5440ec27d5246c5c2eaf7e4cb59c54cba06bfa9
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-07-04 19:45:45 +0300

    feat: Improve login page styling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,6 +5,48 @@ import { Button } from "../components/Button";
 import { TextInput } from "../components/inputs/TextInput";
 import { useUser } from "../hooks/useUser";
 import { User } from "@alpomoney/shared";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const PageContainer = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: "100vh",
+  gap: 32
+});
+
+const FormContainer = styled.div({
+  maxWidth: 400,
+  display: "flex",
+  flexDirection: "column",
+  gap: 32,
+  width: "100%",
+  padding: 32,
+  boxShadow: "0px 5px 25px rgba(0, 0, 0, 0.17)"
+});
+
+const Form = styled.form({
+  display: "flex",
+  flexDirection: "column",
+  gap: 16
+});
+
+const LabelCell = styled.td({
+  textAlign: "right",
+  paddingRight: 16,
+  width: 0
+});
+
+const RegisterLink = styled(Link)({
+  color: "#242424",
+  textDecoration: "none",
+  transitionDuration: "200ms",
+  "&:hover": {
+    color: "black"
+  }
+});
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
@@ -26,24 +68,44 @@ export default function LoginPage() {
     });
   };
 
-  return <div>
-    <h1>Login</h1>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username</label>
-      <TextInput
-        id="username"
-        value={username}
-        onChange={setUsername}
-        placeholder="Username"
-      />
-      <label htmlFor="password">Password</label>
-      <TextInput
-        id="password"
-        value={password}
-        onChange={setPassword}
-        placeholder="Password"
-      />
-      <Button type="submit">Login</Button>
-    </form>
-  </div>;
+  return <PageContainer>
+    <FormContainer>
+      <h1>Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <table>
+          <tbody>
+
+            <tr>
+              <LabelCell>
+                <label htmlFor="username">Username</label>
+              </LabelCell>
+              <td>
+                <TextInput
+                  id="username"
+                  value={username}
+                  onChange={setUsername}
+                  placeholder="Username"
+                />
+              </td>
+            </tr>
+            <tr>
+              <LabelCell>
+                <label htmlFor="password">Password</label>
+              </LabelCell>
+              <td>
+                <TextInput
+                  id="password"
+                  value={password}
+                  onChange={setPassword}
+                  placeholder="Password"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <Button type="submit" variant="filled">Login</Button>
+      </Form>
+    </FormContainer>
+    <RegisterLink to="/register">Don&apos;t have an account yet? Register</RegisterLink>
+  </PageContainer>;
 }
